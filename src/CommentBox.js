a0b43ecdf1d423c7be9ccd0cfd686a32f7bcd2fe
John Deegan <john.deegan@hotmail.com>
2017-06-02 21:14:47 +0100

    get live comments

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -2,21 +2,41 @@
 import React, { Component } from 'react';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
-import DATA from './data';
+import axios from 'axios';
 import style from './style';
 
 class CommentBox extends Component {
     constructor(props){
         super (props);
         this.state = {data: []};
+        this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+        this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+    }
+
+    //get comments from server
+    //it get the url passed in as a prop from index.js
+    loadCommentsFromServer(){
+        axios.get(this.props.url)
+        .then(res => {
+            this.setState({data: res.data});
+        })
+    }
+
+    handleCommentSubmit(comment){
+        //POST request
+    }
+
+    componentDidMount() {
+        this.loadCommentsFromServer();
+        setInterval(this.loadCommentsFromServer, this.props.pollInterval);
     }
 
     render(){
         return (
             <div style={style.commentBox}>
                 <h2>Comments</h2>
-                <CommentList data = {DATA} />
-                <CommentForm />
+                <CommentList data = {this.state.data} />
+                <CommentForm onCommentSubmit={this.handleCommentSubmit} />
             </div>
         );
     }
